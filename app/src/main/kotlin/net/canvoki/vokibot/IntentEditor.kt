a15cebb52412a2fbc88b5d36706c8951189110e4
David García Garzón <voki@canvoki.net>
2026-04-11 05:48:10 +0200

    🐛 IntentEditor: missing import

--- app/src/main/kotlin/net/canvoki/vokibot/IntentEditor.kt
+++ app/src/main/kotlin/net/canvoki/vokibot/IntentEditor.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
