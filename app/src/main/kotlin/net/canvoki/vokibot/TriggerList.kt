aa2d003da61a046512021aecf4d9fc08e856552c
David García Garzón <voki@canvoki.net>
2026-04-30 03:51:36 +0200

    💄 TriggerList: transparent item background

--- app/src/main/kotlin/net/canvoki/vokibot/TriggerList.kt
+++ app/src/main/kotlin/net/canvoki/vokibot/TriggerList.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -25,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -77,6 +79,7 @@ fun TriggerList(
                         tint = MaterialTheme.colorScheme.primary,
                     )
                 },
+                colors = ListItemDefaults.colors(containerColor = Color.Transparent),
                 modifier =
                     Modifier.clickable {
                         nav.pop(Pair(trigger.type, trigger.id))
