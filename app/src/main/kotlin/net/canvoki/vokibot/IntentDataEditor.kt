9d657514a49251514ae81726a12ea7e263e62d74
David García Garzón <voki@canvoki.net>
2026-05-24 07:19:16 +0200

    🚧 IntentDataEditor: added fields for uri and mime

--- app/src/main/kotlin/net/canvoki/vokibot/IntentDataEditor.kt
+++ app/src/main/kotlin/net/canvoki/vokibot/IntentDataEditor.kt
@@ -1,7 +1,14 @@
 package net.canvoki.vokibot
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun IntentDataEditor(
@@ -12,5 +19,19 @@ fun IntentDataEditor(
 ) {
     Column {
         SectionHeader("Data")
+
+        OutlinedTextField(
+            value = dataUri ?: "",
+            onValueChange = { onDataChanged(it.ifBlank { null }) },
+            label = { Text("URI") },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(
+            value = mimeType ?: "",
+            onValueChange = { onMimeChanged(it.ifBlank { null }) },
+            label = { Text("MIME type") },
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
