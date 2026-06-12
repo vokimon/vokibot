5fae0575149ae8f5cc193314f91f80da25ca70c9
David García Garzón <voki@canvoki.net>
2026-06-12 04:16:48 +0200

    💄 data exchange options share backgroudn with parent

--- app/src/main/kotlin/net/canvoki/vokibot/ExportOption.kt
+++ app/src/main/kotlin/net/canvoki/vokibot/ExportOption.kt
@@ -3,10 +3,12 @@ package net.canvoki.vokibot
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -21,6 +23,7 @@ fun ExportOption() {
     ListItem(
         headlineContent = { Text(stringResource(R.string.dasher_export_title)) },
         supportingContent = { Text(stringResource(R.string.dasher_export_description)) },
+        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
         leadingContent = {
             Icon(
                 painter = painterResource(R.drawable.ic_file_upload),
--- app/src/main/kotlin/net/canvoki/vokibot/ImportOption.kt
+++ app/src/main/kotlin/net/canvoki/vokibot/ImportOption.kt
@@ -3,6 +3,7 @@ package net.canvoki.vokibot
 import androidx.compose.foundation.clickable
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -28,6 +30,7 @@ fun ImportOption() {
     ListItem(
         headlineContent = { Text(stringResource(R.string.dasher_import_title)) },
         supportingContent = { Text(stringResource(R.string.dasher_import_description)) },
+        colors = ListItemDefaults.colors(containerColor = Color.Transparent),
         leadingContent = {
             Icon(
                 painter = painterResource(R.drawable.ic_file_download),
