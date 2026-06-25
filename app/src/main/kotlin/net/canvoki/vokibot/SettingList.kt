93285c90620ad04db1dc689b50caa262bcbd75a4
David García Garzón <voki@canvoki.net>
2026-06-25 09:45:13 +0200

    ✨ list of settings

--- app/src/main/kotlin/net/canvoki/vokibot/SettingList.kt
+++ app/src/main/kotlin/net/canvoki/vokibot/SettingList.kt
@@ -1,55 +1,55 @@
 package net.canvoki.vokibot
 
-import android.provider.Settings
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import kotlinx.serialization.Serializable
+import net.canvoki.shared.component.AsyncList
 import net.canvoki.shared.component.StackNavigatorState
 import net.canvoki.shared.component.StackedScreen
 import net.canvoki.vokibot.common.EditorHeader
+import net.canvoki.vokibot.common.ListGroupHeader
 
 @Serializable
 data object SettingList : StackedScreen<String>() {
     @Composable
     override fun Screen(nav: StackNavigatorState) {
-        SettingListScreen(nav)
+        SettingList(nav)
     }
 }
 
 @Composable
-fun SettingListScreen(nav: StackNavigatorState) {
-    Column(
-        modifier =
-            Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
+fun SettingList(nav: StackNavigatorState) {
+    Column(modifier = Modifier.fillMaxSize()) {
         EditorHeader(
             icon = painterResource(ChangeSettingCommand.iconRes),
             title = stringResource(ChangeSettingCommand.labelRes),
         )
-
-        ListItem(
-            headlineContent = { Text("Adaptive Brightness") },
-            supportingContent = { Text("SCREEN_BRIGHTNESS_MODE") },
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .clickable {
-                        nav.pop(Settings.System.SCREEN_BRIGHTNESS_MODE)
-                    },
-        )
+        AsyncList(
+            loader = { SETTING_VALUES },
+            itemKey = { it.id },
+            groupBy = { it.category.name },
+            headerContent = { key ->
+                val category = SettingCategory.valueOf(key)
+                ListGroupHeader(title = stringResource(category.labelRes))
+            },
+            notFoundMessage = "No settings found",
+        ) { setting ->
+            ListItem(
+                headlineContent = { Text(setting.name) },
+                supportingContent = { Text(setting.description) },
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .clickable { nav.pop(setting.id) },
+            )
+        }
     }
 }
